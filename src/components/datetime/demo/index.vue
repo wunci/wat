df31fdf5c76f0ac6d71e0d33007922ab618579e7
wclimb <875246904@qq.com>
2019-03-13 14:04:14 +0800

    update: datetime and docs

--- src/components/datetime/demo/index.vue
+++ src/components/datetime/demo/index.vue
@@ -4,11 +4,56 @@
     <datetime
       v-model="datetimeValue"
       :title="'时间选择器'"
-      max-year="2022"
       @on-change="popupPickerChange"
     >
       <div class="ml-20">
-        <wat-button>时间选择器</wat-button>
+        <wat-button>默认</wat-button>
+      </div>
+    </datetime>
+    <datetime
+      v-model="datetimeValue"
+      :title="'时间选择器'"
+      max-year="2022"
+      type="YYYY-MM-DD hh:mm"
+      @on-change="popupPickerChange"
+    >
+      <div class="ml-20">
+        <wat-button>YYYY-MM-DD hh:mm</wat-button>
+      </div>
+    </datetime>
+    <datetime
+      v-model="datetimeValue"
+      :title="'时间选择器'"
+      max-year="2022"
+      max-month="10"
+      max-days="20"
+      min-days="10"
+      type="YYYY-MM-DD hh"
+      @on-change="popupPickerChange"
+    >
+      <div class="ml-20">
+        <div style="margin-bottom:10px">
+          // max-year="2022" max-month="10" mmin-days="20" max-days="20"
+        </div>
+        <wat-button>YYYY-MM-DD hh</wat-button>
+      </div>
+    </datetime>
+    <datetime
+      v-model="datetimeValue"
+      :title="'时间选择器'"
+      max-year="2022"
+      type="hh:mm"
+      max-hours="17"
+      min-hours="9"
+      min-minutes="10"
+      max-minutes="40"
+      @on-change="popupPickerChange"
+    >
+      <div class="ml-20">
+        <div style="margin-bottom:10px">
+          // min-hours="9" max-hours="17" min-minutes="10" max-minutes="40"
+        </div>
+        <wat-button>hh:mm</wat-button>
       </div>
     </datetime>
   </div>
